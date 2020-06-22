dcba3894ba79b7b3cde5aa4191e32eecd677042b
ヨン ソジョン <yeonsojeong@yonuiMaBookpuro.elecom>
2020-06-22 11:51:27 +0900

    jankenvue

--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,11 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    count: 0
   },
   mutations: {
+    increment: state => state.count++,
+    decrement: state => state.count--
   },
   actions: {
   },
